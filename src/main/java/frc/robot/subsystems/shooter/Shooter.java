40b4aa924d94f8cefece9aeb75b051c0291e3c90
Jason <118124215+rocketbooster1000@users.noreply.github.com>
2024-11-22 15:45:15 -0700

    shoter slotconfigs

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -1,8 +1,13 @@
 package frc.robot.subsystems.shooter;
 
 import com.ctre.phoenix6.hardware.TalonFX;
+import com.ctre.phoenix6.signals.InvertedValue;
+import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.configs.MotorOutputConfigs;
+import com.ctre.phoenix6.configs.Slot0Configs;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Utils3006.SmartDashboardNumber;
 
 public class Shooter extends SubsystemBase{
     private static Shooter instance = null;
@@ -10,11 +15,30 @@ public class Shooter extends SubsystemBase{
     private TalonFX m_shooterMotor = new TalonFX(39);
     private TalonFX m_hoodMotor = new TalonFX(38);
 
+    private Slot0Configs slot0Configs = new Slot0Configs();
+
+    private SmartDashboardNumber shooterKs = new SmartDashboardNumber("shooter/ks", 0);
+    private SmartDashboardNumber shooterKa = new SmartDashboardNumber("shooter/ka", 0);
+    private SmartDashboardNumber shooterKv = new SmartDashboardNumber("shooter/kv", 0); //to be tuned;
+    private SmartDashboardNumber shooterKp = new SmartDashboardNumber("shooter/kp", 0); //to be tuned;
+    private SmartDashboardNumber shooterKi = new SmartDashboardNumber("shooter/ki", 0);
+    private SmartDashboardNumber shooterKd = new SmartDashboardNumber("shooter/kd", 0);
+
     private double targetHoodAngle;
     private double targetVelocityRPM;
 
     private Shooter() {
         super("Shooter");
+
+        this.m_shooterMotor.getConfigurator().apply(
+            new MotorOutputConfigs()
+                .withInverted(InvertedValue.Clockwise_Positive)
+                .withPeakForwardDutyCycle(1d)
+                .withPeakReverseDutyCycle(-1d)
+                .withNeutralMode(NeutralModeValue.Brake)
+        );
+
+        this.m_shooterMotor.getConfigurator().apply(slot0Configs);
     }
 
     public void setHoodAngle(double angle) {
