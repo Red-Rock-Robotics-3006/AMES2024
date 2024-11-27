556f34207b688b232365227b12d43fc3d7259b76
Jason <118124215+rocketbooster1000@users.noreply.github.com>
2024-11-27 14:33:51 -0700

    missing shooter code

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -3,19 +3,36 @@ package frc.robot.subsystems.shooter;
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.ctre.phoenix6.signals.InvertedValue;
 import com.ctre.phoenix6.signals.NeutralModeValue;
+import com.ctre.phoenix6.Utils;
+import com.ctre.phoenix6.configs.MotionMagicConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.configs.Slot0Configs;
+import com.ctre.phoenix6.controls.CoastOut;
+import com.ctre.phoenix6.controls.DutyCycleOut;
+import com.ctre.phoenix6.controls.MotionMagicVelocityVoltage;
+import com.ctre.phoenix6.controls.PositionVoltage;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Utils3006.SmartDashboardNumber;
 
 public class Shooter extends SubsystemBase{
     private static Shooter instance = null;
 
+    public static double kMaxHoodAngle, 
+                         kMinHoodAngle;
+    public static double kMaxHoodRotation, 
+                         kMinHoodRotation;
+
     private TalonFX m_shooterMotor = new TalonFX(39);
     private TalonFX m_hoodMotor = new TalonFX(38);
 
-    private Slot0Configs slot0Configs = new Slot0Configs();
+    private Slot0Configs shooterSlot0Configs = new Slot0Configs();
+    private Slot0Configs hoodSlot0Configs = new Slot0Configs();
+
+    private MotionMagicConfigs shooterMotionMagicConfigs = new MotionMagicConfigs();
+
+    private SmartDashboardNumber shooterAccel = new SmartDashboardNumber("shooter/accel-motion-magic", 0);
 
     private SmartDashboardNumber shooterKs = new SmartDashboardNumber("shooter/ks", 0);
     private SmartDashboardNumber shooterKa = new SmartDashboardNumber("shooter/ka", 0);
@@ -24,29 +41,139 @@ public class Shooter extends SubsystemBase{
     private SmartDashboardNumber shooterKi = new SmartDashboardNumber("shooter/ki", 0);
     private SmartDashboardNumber shooterKd = new SmartDashboardNumber("shooter/kd", 0);
 
+    private SmartDashboardNumber hoodKs = new SmartDashboardNumber("hood/ks", 0);
+    private SmartDashboardNumber hoodKa = new SmartDashboardNumber("hood/ka", 0);
+    private SmartDashboardNumber hoodKv = new SmartDashboardNumber("hood/kv", 0); //to be tuned;
+    private SmartDashboardNumber hoodKp = new SmartDashboardNumber("hood/kp", 0); //to be tuned;
+    private SmartDashboardNumber hoodKi = new SmartDashboardNumber("hood/ki", 0);
+    private SmartDashboardNumber hoodKd = new SmartDashboardNumber("hood/kd", 0);
+
+    //for smartdashboard only
     private double targetHoodAngle;
-    private double targetVelocityRPM;
+    private double targetRPM;
 
     private Shooter() {
         super("Shooter");
 
         this.m_shooterMotor.getConfigurator().apply(
             new MotorOutputConfigs()
-                .withInverted(InvertedValue.Clockwise_Positive)
+                .withInverted(InvertedValue.CounterClockwise_Positive)
                 .withPeakForwardDutyCycle(1d)
                 .withPeakReverseDutyCycle(-1d)
                 .withNeutralMode(NeutralModeValue.Brake)
         );
 
-        this.m_shooterMotor.getConfigurator().apply(slot0Configs);
+        this.m_hoodMotor.getConfigurator().apply(
+            new MotorOutputConfigs()
+                .withInverted(InvertedValue.CounterClockwise_Positive)
+                .withPeakForwardDutyCycle(1d)
+                .withPeakReverseDutyCycle(1d)
+                .withNeutralMode(NeutralModeValue.Brake)
+        );
+
+        this.shooterSlot0Configs = new Slot0Configs()
+            .withKS(shooterKs.getNumber())
+            .withKA(shooterKa.getNumber())
+            .withKV(shooterKv.getNumber())
+            .withKP(shooterKp.getNumber())
+            .withKI(shooterKi.getNumber())
+            .withKD(shooterKd.getNumber());
+
+        this.hoodSlot0Configs = new Slot0Configs()
+            .withKS(hoodKs.getNumber())
+            .withKA(hoodKa.getNumber())
+            .withKV(hoodKv.getNumber())
+            .withKP(hoodKp.getNumber())
+            .withKI(hoodKi.getNumber())
+            .withKD(hoodKd.getNumber());
+
+        this.shooterMotionMagicConfigs = new MotionMagicConfigs()
+            .withMotionMagicAcceleration(shooterAccel.getNumber());
+
+        this.m_shooterMotor.getConfigurator().apply(shooterSlot0Configs);
+        this.m_shooterMotor.getConfigurator().apply(shooterMotionMagicConfigs);
+        this.m_hoodMotor.getConfigurator().apply(hoodSlot0Configs);
     }
 
     public void setHoodAngle(double angle) {
-
+        this.m_hoodMotor.setControl(new PositionVoltage(this.angleToRotation(angle))
+                                        .withSlot(0)
+                                        .withEnableFOC(true)
+                                        .withOverrideBrakeDurNeutral(true)
+        );
+        this.targetHoodAngle = angle;
     }
 
-    public void setVelocity(double velocity) {
-        
+    public void setShooterRPM(double rpm) {
+        if (Double.compare(rpm, 0d) == 0) {
+            this.m_shooterMotor.setControl(new DutyCycleOut(0d).withOverrideBrakeDurNeutral(false));
+        } else {
+            this.m_shooterMotor.setControl(new MotionMagicVelocityVoltage(rpm / 60d)
+                                            .withSlot(0)
+                                            .withEnableFOC(true)
+                                            .withOverrideBrakeDurNeutral(true)
+            );
+        }
+        this.targetRPM = rpm;
+    }
+
+    public void resetHood() {
+        this.m_hoodMotor.setControl(new CoastOut());
+        this.m_hoodMotor.setPosition(0d);
+    }
+
+    public double angleToRotation(double angle) {
+        return ((kMaxHoodRotation - kMinHoodRotation) / (kMaxHoodAngle - kMinHoodAngle)) * (angle - kMinHoodAngle) + kMinHoodRotation;
+    }
+
+    @Override
+    public void periodic() {
+        if (shooterKs.hasChanged()
+        || shooterKv.hasChanged()
+        || shooterKp.hasChanged()
+        || shooterKi.hasChanged()
+        || shooterKd.hasChanged()
+        || shooterKa.hasChanged()) {
+            shooterSlot0Configs.kS = shooterKs.getNumber();
+            shooterSlot0Configs.kV = shooterKv.getNumber();
+            shooterSlot0Configs.kP = shooterKp.getNumber();
+            shooterSlot0Configs.kI = shooterKi.getNumber();
+            shooterSlot0Configs.kD = shooterKd.getNumber();
+            shooterSlot0Configs.kA = shooterKa.getNumber();
+
+            if (!Utils.isSimulation()) this.m_shooterMotor.getConfigurator().apply(shooterSlot0Configs);
+            System.out.println("applyied");
+        }
+
+        if (shooterAccel.hasChanged()) {
+            shooterMotionMagicConfigs.MotionMagicAcceleration = shooterAccel.getNumber();
+            this.m_shooterMotor.getConfigurator().apply(shooterMotionMagicConfigs);
+        }
+
+        SmartDashboard.putNumber("shooter/acceleration", this.m_shooterMotor.getAcceleration().getValueAsDouble());
+        SmartDashboard.putNumber("shooter/velocity", this.m_shooterMotor.getVelocity().getValueAsDouble());
+        SmartDashboard.putNumber("shooter/rpm-target", this.targetRPM);
+
+        if (hoodKs.hasChanged()
+            || hoodKv.hasChanged()
+            || hoodKp.hasChanged()
+            || hoodKi.hasChanged()
+            || hoodKd.hasChanged()
+            || hoodKa.hasChanged()) {
+            hoodSlot0Configs.kS = hoodKs.getNumber();
+            hoodSlot0Configs.kV = hoodKv.getNumber();
+            hoodSlot0Configs.kP = hoodKp.getNumber();
+            hoodSlot0Configs.kI = hoodKi.getNumber();
+            hoodSlot0Configs.kD = hoodKd.getNumber();
+            hoodSlot0Configs.kA = hoodKa.getNumber();
+
+            if (!Utils.isSimulation()) this.m_hoodMotor.getConfigurator().apply(hoodSlot0Configs);
+            System.out.println("applyied");
+        }
+
+        SmartDashboard.putNumber("hood/position", this.m_hoodMotor.getPosition().getValueAsDouble());
+        SmartDashboard.putNumber("hood/hood-target-angle", this.targetHoodAngle);
+
     }
 
     public static Shooter getInstance() {
